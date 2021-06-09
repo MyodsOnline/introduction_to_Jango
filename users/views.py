d1bc7ace42dde6b902db7720d2439f683235ae3f
Andrew Smith <diver.vlz@gmail.com>
2021-06-09 23:00:26 +0300

    lesson_5. found some internet. First hour of the webinar has gone and the Basket app has been created

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from basket.models import Basket
 
 
 # Create your views here.
@@ -27,13 +28,28 @@ def register(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегестрировались')
             return HttpResponseRedirect(reverse('users:login'))
-        else:
-            print(form.errors)
     else:
         form = UserRegistrationForm()
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=user)
+    context = {'title': 'GeekShop - Личный кабинет', 'form': form,
+               'basket': Basket.objects.all()}
+    return render(request, 'users/profile.html', context)
 
 def logout(request):
     auth.logout(request)
