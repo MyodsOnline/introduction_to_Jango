52e0e6869414349b7a3a63546a173b68749a138d
Andrew Smith <diver.vlz@gmail.com>
2021-06-14 11:43:35 +0300

    lesson_6.

--- users/views.py
+++ users/views.py
@@ -49,8 +49,23 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=user)
-    context = {'title': 'GeekShop - Личный кабинет', 'form': form,
-               'baskets': Basket.objects.filter(user=user),}
+
+    # baskets = Basket.objects.filter(user=user) // отсавлю здесь как напоминание
+    # total_quantity = 0
+    # total_sum = 0
+    # for basket in baskets:
+    #     total_quantity += basket.quantity
+    #     total_sum += basket.sum()
+
+    # total_quantity = sum(basket.quantity for basket in baskets)
+    # total_sum = sum(basket.sum() for basket in baskets)
+
+    context = {'title': 'GeekShop - Личный кабинет',
+               'form': form,
+               'baskets': Basket.objects.filter(user=user),
+               # 'total_quantity': sum(basket.quantity for basket in baskets),
+               # 'total_sum': sum(basket.sum() for basket in baskets),
+               }
     return render(request, 'users/profile.html', context)
 
 
