43004f0fcacee9b46a61fc85557c1dcbf863f15b
Andrew Smith <diver.vlz@gmail.com>
2021-06-20 13:18:08 +0300

    lesson_8. added Paginator to products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from products.models import ProductCategory, Product
 
@@ -11,14 +12,25 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {
         'title': 'GeekShop Products',
         'categories': ProductCategory.objects.all(),
         'date': timezone.now().date(),
     }
-    if category_id:
-        context['products'] = Product.objects.filter(category_id=category_id)
-    else:
-        context['products'] = Product.objects.all()
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    paginator = Paginator(products, 3)
+
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
+    # if category_id:
+    #     context['products'] = Product.objects.filter(category_id=category_id)
+    # else:
+    #     context['products'] = Product.objects.all()
     return render(request, 'products/products.html', context)
