ccc66aa8311a3a819d9459c2ea53c26e5dd3d1f7
Andrew Smith <diver.vlz@gmail.com>
2021-06-20 15:14:54 +0300

    lesson_8. webinar topics complete

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, UserListView, admin_users_update, admin_users_delete, CreateUser
+from admins.views import index, UserListView, UserUpdateView, UserDeleteView, CreateUser
 
 app_name = 'admins'
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users/create/', CreateUser.as_view(), name='admin_users_create'),
-    path('users/update/<int:id>/', admin_users_update, name='admin_users_update'),
-    path('users/delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
+    path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),  # передали primary key
+    path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
 ]
